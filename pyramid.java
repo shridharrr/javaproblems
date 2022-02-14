99b85b658c446d5b350d163f2229b59a118a8514
shridharrr <98966222+shridharrr@users.noreply.github.com>
2022-02-14 15:58:37 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,18 @@
+package src;
+
+public class pyramid {
+	
+	public static void main(String[] arg) {
+	
+	  int rows = 5;
+
+	    for (int i = 1; i <= rows; ++i) {
+	      for (int j = 1; j <= i; ++j) {
+	        System.out.print("* ");
+	      }
+	      System.out.println();
+	    }
+	  }
+	}
+
+
---
+++ shri.java
@@ -0,0 +1,22 @@
+package src;
+
+public class shri {
+	
+	 public static void main(String[] arg) {
+	
+		 int pencil = 10;
+	
+		 int pen = 60;
+	
+		 int book = 50;
+	
+	 double totalamount = pencil + pen + book;
+	 double totaldiscount= totalamount - 0.1*totalamount;
+	 double totalpay = totaldiscount + 0.05*totaldiscount;
+	      System.out.println(totalpay); 
+	 
+	 
+	 }} 
+
+	 
+
