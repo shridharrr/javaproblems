46868e435b22ee6e750a8a5d36a1e0a14ec2e3f1
shridharrr <98966222+shridharrr@users.noreply.github.com>
2022-02-16 12:18:16 +0530

    Add files via upload

---
+++ matrixs.java
@@ -0,0 +1,45 @@
+package scr;
+
+public class matrixs {
+	
+	public static void main(String[] args) {
+		
+		int a[][] = {{2,3,4},{2,3,4},{6,7,8}};
+		int b[][] = {{3,5,6},{4,6,4},{7,9,8}};
+		
+		int c[][] = new int [3][3];
+		
+		for(int i=0;i<3;i++) {
+	    for(int j=0;i<3;j++) {
+	    	
+	    	
+	    	c[i][j] = a[i][j]+b[i][j];
+	    	
+	    	System.out.println(c[i][j]);
+	    }
+	    
+	    
+			
+		}
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
